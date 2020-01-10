184889ae4c78d9d039835ea2867a9c1e6658fad0
Gica Gugui <gica.gugui@gmail.com>
2020-01-10 15:20:28 +0200

    Initial Commit

---
+++ Whim/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Whim
+//
+//  Created by Gica Gugui on 10/01/2020.
+//  Copyright © 2020 Gica Gugui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WhimTests/WhimTests.swift
@@ -0,0 +1,34 @@
+//
+//  WhimTests.swift
+//  WhimTests
+//
+//  Created by Gica Gugui on 10/01/2020.
+//  Copyright © 2020 Gica Gugui. All rights reserved.
+//
+
+import XCTest
+@testable import Whim
+
+class WhimTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
