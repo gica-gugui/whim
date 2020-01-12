940178f3418076cdb463df0a8434e129eec11b57
Gica Gugui <gica.gugui@gmail.com>
2020-01-12 16:22:59 +0200

    Details modal view controller: wip.

--- Whim/ViewControllers/Details/DetailsViewController.swift
+++ Whim/ViewControllers/Details/DetailsViewController.swift
@@ -13,13 +13,30 @@ class DetailsViewController: BaseViewController {
     @IBOutlet weak var dimmerView: UIView!
     @IBOutlet weak var cardView: UIView!
     @IBOutlet weak var cardViewTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var handleView: UIView!
     
     var backingImage: UIImage?
+    
+    enum CardViewState {
+        case expanded
+        case normal
+    }
+    
+    // default card view state is normal
+    var cardViewState : CardViewState = .normal
+
+    // to store the card view top constraint value before the dragging start
+    // default is 30 pt from safe area top
+    var cardPanStartingTopConstant : CGFloat = 30.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         backingImageView.image = backingImage
+        
+        // round the handle view
+        handleView.clipsToBounds = true
+        handleView.layer.cornerRadius = 3.0
         
         // round the top left and top right corner of card view
         cardView.clipsToBounds = true
@@ -38,6 +55,16 @@ class DetailsViewController: BaseViewController {
         let dimmerTap = UITapGestureRecognizer(target: self, action: #selector(dimmerViewTapped(_:)))
         dimmerView.addGestureRecognizer(dimmerTap)
         dimmerView.isUserInteractionEnabled = true
+        
+        // add pan gesture recognizer to the view controller's view (the whole screen)
+        let viewPan = UIPanGestureRecognizer(target: self, action: #selector(viewPanned(_:)))
+        
+        // by default iOS will delay the touch before recording the drag/pan information
+        // we want the drag gesture to be recorded down immediately, hence setting no delay
+        viewPan.delaysTouchesBegan = false
+        viewPan.delaysTouchesEnded = false
+
+        self.view.addGestureRecognizer(viewPan)
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -50,6 +77,43 @@ class DetailsViewController: BaseViewController {
         hideCardAndGoBack()
     }
     
+    @IBAction func viewPanned(_ panRecognizer: UIPanGestureRecognizer) {
+      let velocity = panRecognizer.velocity(in: self.view)
+      let translation = panRecognizer.translation(in: self.view)
+      
+      switch panRecognizer.state {
+      case .began:
+        cardPanStartingTopConstant = cardViewTopConstraint.constant
+        
+      case .changed:
+        if self.cardPanStartingTopConstant + translation.y > 30.0 {
+          self.cardViewTopConstraint.constant = self.cardPanStartingTopConstant + translation.y
+        }
+        
+        // change the dimmer view alpha based on how much user has dragged
+        dimmerView.alpha = dimAlphaWithCardTopConstraint(value: self.cardViewTopConstraint.constant)
+
+      case .ended:
+        if velocity.y > 1500.0 {
+          hideCardAndGoBack()
+          return
+        }
+        
+        if let safeAreaHeight = UIApplication.shared.keyWindow?.safeAreaLayoutGuide.layoutFrame.size.height,
+          let bottomPadding = UIApplication.shared.keyWindow?.safeAreaInsets.bottom {
+          
+          if self.cardViewTopConstraint.constant < (safeAreaHeight + bottomPadding) * 0.25 {
+            showCard(atState: .expanded)
+          } else if self.cardViewTopConstraint.constant < (safeAreaHeight) - 70 {
+            showCard(atState: .normal)
+          } else {
+            hideCardAndGoBack()
+          }
+        }
+      default:
+        break
+      }
+    }
     private func hideCardAndGoBack() {
           
         // ensure there's no pending layout changes before animation runs
@@ -91,7 +155,8 @@ class DetailsViewController: BaseViewController {
         hideCard.startAnimation()
     }
 
-    private func showCard() {
+    // default to show card at normal state, if showCard() is called without parameter
+    private func showCard(atState: CardViewState = .normal) {
        
       // ensure there's no pending layout changes before animation runs
       self.view.layoutIfNeeded()
@@ -102,12 +167,17 @@ class DetailsViewController: BaseViewController {
       if let safeAreaHeight = UIApplication.shared.keyWindow?.safeAreaLayoutGuide.layoutFrame.size.height,
         let bottomPadding = UIApplication.shared.keyWindow?.safeAreaInsets.bottom {
         
-        // when card state is normal, its top distance to safe area is
-        // (safe area height + bottom inset) / 2.0
-        cardViewTopConstraint.constant = (safeAreaHeight + bottomPadding) / 2.0
+        if atState == .expanded {
+          // if state is expanded, top constraint is 30pt away from safe area top
+          cardViewTopConstraint.constant = 30.0
+        } else {
+          cardViewTopConstraint.constant = (safeAreaHeight + bottomPadding) / 2.0
+        }
+        
+        cardPanStartingTopConstant = cardViewTopConstraint.constant
       }
       
-      // move card up from bottom by telling the app to refresh the frame/position of view
+      // move card up from bottom
       // create a new property animator
       let showCard = UIViewPropertyAnimator(duration: 0.25, curve: .easeIn, animations: {
         self.view.layoutIfNeeded()
@@ -115,11 +185,44 @@ class DetailsViewController: BaseViewController {
       
       // show dimmer view
       // this will animate the dimmerView alpha together with the card move up animation
-      showCard.addAnimations({
+      showCard.addAnimations {
         self.dimmerView.alpha = 0.7
-      })
+      }
       
       // run the animation
       showCard.startAnimation()
+    }
+    
+    private func dimAlphaWithCardTopConstraint(value: CGFloat) -> CGFloat {
+      let fullDimAlpha : CGFloat = 0.7
+      
+      // ensure safe area height and safe area bottom padding is not nil
+      guard let safeAreaHeight = UIApplication.shared.keyWindow?.safeAreaLayoutGuide.layoutFrame.size.height,
+        let bottomPadding = UIApplication.shared.keyWindow?.safeAreaInsets.bottom else {
+        return fullDimAlpha
+      }
+      
+      // when card view top constraint value is equal to this,
+      // the dimmer view alpha is dimmest (0.7)
+      let fullDimPosition = (safeAreaHeight + bottomPadding) / 2.0
+      
+      // when card view top constraint value is equal to this,
+      // the dimmer view alpha is lightest (0.0)
+      let noDimPosition = safeAreaHeight + bottomPadding
+      
+      // if card view top constraint is lesser than fullDimPosition
+      // it is dimmest
+      if value < fullDimPosition {
+        return fullDimAlpha
+      }
+      
+      // if card view top constraint is more than noDimPosition
+      // it is dimmest
+      if value > noDimPosition {
+        return 0.0
+      }
+      
+      // else return an alpha value in between 0.0 and 0.7 based on the top constraint value
+      return fullDimAlpha * 1 - ((value - fullDimPosition) / fullDimPosition)
     }
 }
